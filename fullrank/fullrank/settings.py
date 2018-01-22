82de84fb8f7385fc795e58e1449bcb888d749173
Matthew Gan <matthewgan@126.com>
2018-01-22 21:44:31 +0800

    change installed app index

--- fullrank/fullrank/settings.py
+++ fullrank/fullrank/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "101.132.174.202", "fullrank.cc", "ww
 # Application definition
 
 INSTALLED_APPS = [
+    'iotCenter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iotCenter',
 ]
 
 MIDDLEWARE = [
